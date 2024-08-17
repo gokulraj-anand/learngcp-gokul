6f24979ee00829a1f054c45fe2227bb105d43d64
gokulraj-anand <gokulraj.anand@gmail.com>
2024-08-17 15:56:04 +0530

    Create VEHICLE_create_tbl_sales_agency.sql

---
+++ gcp-bigquery/ddl/VEHICLE_create_tbl_sales_agency.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `learngcp-gokul.bqdataset.tbl_sales_agency` (
+    agency_id STRING NOT NULL,
+    agency_name STRING,
+    agency_address STRING,
+    agency_city STRING,
+    agency_pincode STRING
+);
