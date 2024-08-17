61f11ff455ef7b19ee82c615026721252f9d4d5f
gokulraj-anand <gokulraj.anand@gmail.com>
2024-08-17 15:58:27 +0530

    Create VEHICLE_insert_tbl_sales_agency.sql

---
+++ gcp-bigquery/dml/VEHICLE_insert_tbl_sales_agency.sql
@@ -0,0 +1,7 @@
+INSERT INTO `learngcp-gokul.bqdataset.tbl_sales_agency` (agency_id, agency_name, agency_address, agency_city, agency_pincode)
+VALUES
+    ('BNG_STH_HND1', 'Bigwing Honda', '10, Orchard St, Banashankari', 'Bengaluru', '560004'),
+    ('BNG_STH_SZK1', 'Pramaan Suzuki', '12, Orchard St, Banashankari', 'Bengaluru', '560004'),
+    ('BNG_STH_SZK2', 'Advaith Suzuki', '18, 2nd Main, Jayanagar', 'Bengaluru', '560006'),
+    ('BNG_STH_BJJI1', 'Praveen Bajaj', '266/1, 1st Main, Jayanagar', 'Bengaluru', '560006'),
+    ('BNG_NTH_HND2', 'Servo Honda', '21, Orchard St, Rajajinagar', 'Bengaluru', '560020');
