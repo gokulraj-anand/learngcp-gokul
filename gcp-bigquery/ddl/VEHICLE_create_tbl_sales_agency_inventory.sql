8c2ec9fb6444041177a89dbced2c41937ed5a604
gokulraj-anand <gokulraj.anand@gmail.com>
2024-08-17 16:03:43 +0530

    Update VEHICLE_create_tbl_sales_agency_inventory.sql

--- gcp-bigquery/ddl/VEHICLE_create_tbl_sales_agency_inventory.sql
+++ gcp-bigquery/ddl/VEHICLE_create_tbl_sales_agency_inventory.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `your_project.your_dataset.tbl_sales_agency_inventory` (
+CREATE TABLE `learngcp-gokul.bqdataset.tbl_sales_agency_inventory` (
     agency_id STRING NOT NULL,
     vehicle_id STRING NOT NULL,
     agency_inventory INT64
