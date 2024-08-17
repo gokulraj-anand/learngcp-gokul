fa99fe81ae1e429913ad9892498446f6cb16420c
gokulraj-anand <gokulraj.anand@gmail.com>
2024-08-17 16:03:16 +0530

    Create VEHICLE_insert_tbl_sales_agency_inventory.sql

---
+++ gcp-bigquery/dml/VEHICLE_insert_tbl_sales_agency_inventory.sql
@@ -0,0 +1,13 @@
+INSERT INTO `learngcp-gokul.bqdataset.tbl_sales_agency_inventory` (agency_id, vehicle_id, agency_inventory)
+VALUES
+    ('BNG_STH_HND1', 'honda_dio_2016', 500),
+    ('BNG_NTH_HND2', 'honda_dio_2016', 2000),
+    ('BNG_NTH_HND2', 'honda_activa_2021', 11000),
+    ('BNG_STH_HND1', 'honda_activa_2021', 11000),
+    ('BNG_NTH_HND2', 'honda_cb350rs_2023', 3000),
+    ('BNG_STH_HND1', 'honda_cb350rs_2023', 7000),
+    ('BNG_STH_BJJI1', 'bajaj_pulsar200_2018', 6000),
+    ('BNG_STH_SZK2', 'suzuki_access125_2019', 4000),
+    ('BNG_STH_SZK1', 'suzuki_access125_2019', 5000),
+    ('BNG_STH_SZK1', 'suzuki_access125_2021', 18000),
+    ('BNG_STH_SZK2', 'suzuki_access125_2021', 16000);
